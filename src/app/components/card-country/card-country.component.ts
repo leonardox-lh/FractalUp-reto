8287d6b667853999350b2f0e96ec24414eb890e8
leonardo <u202124304@upc.edu.pe>
2024-12-13 15:51:16 -0500

    feat(searching): Add card country

---
+++ src/app/components/card-country/card-country.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-country',
+  templateUrl: './card-country.component.html',
+  styleUrls: ['./card-country.component.css']
+})
+export class CardCountryComponent {
+
+}
