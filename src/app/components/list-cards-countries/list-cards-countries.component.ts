4a1142937c1dc2339ba87b771b91b5d6bba068b6
leonardo <u202124304@upc.edu.pe>
2024-12-14 14:17:22 -0500

    feat(searching): update list card, added filters dynamics

--- src/app/components/list-cards-countries/list-cards-countries.component.ts
+++ src/app/components/list-cards-countries/list-cards-countries.component.ts
@@ -12,15 +12,19 @@ import {CountriesImgFlagService} from "../../services/countries-img-flag.service
 })
 export class ListCardsCountriesComponent implements OnInit{
   countries: Country[] = [];
-
+  filteredCountries: Country[] = [];
+  searchTerm: string = '';
   selectedCountry: any = null;
   isShowCountry: boolean = false;
+  selectedContinents: string[] = [];
+  isLoading: boolean = false;
   constructor(private countriesService: CountriesService,
               private countriesImgService: CountriesImgService,
               private countriesImgFlagService: CountriesImgFlagService
   ) {}
 
   ngOnInit(): void {
+    this.isLoading = true;
     this.countriesService.getCountries().subscribe((result: any) => {
       this.countries = result.data.countries.slice(0, 20);
       this.fetchFlags(this.countries);
@@ -28,6 +32,18 @@ export class ListCardsCountriesComponent implements OnInit{
     });
   }
 
+  filterCountries(): void {
+    this.filteredCountries = this.countries.filter(country =>
+        country.name.toLowerCase().includes(this.searchTerm.toLowerCase()) &&
+        (this.selectedContinents.length > 0
+            ? this.selectedContinents.some(continent => country.continent.name.toLowerCase().includes(continent.toLowerCase()))
+            : true)
+    );
+  }
+  onSearchChange(searchTerm: string): void {
+    this.searchTerm = searchTerm;
+    this.filterCountries(); // Llama a la función de filtrado
+  }
   fetchFlags(countries: any[]): void {
     this.countries = countries.map((country) => {
       const flagUrl = this.countriesImgFlagService.searchFlag(country.code);
@@ -40,36 +56,50 @@ export class ListCardsCountriesComponent implements OnInit{
   }
 
   fetchImgCountry(countries: any[]): void {
+    // Crear una lista de observables para obtener las imágenes
     const requests = countries.map((country) =>
-      this.countriesImgService.searchImg(country.name).pipe(
-        catchError((error) => {
-          console.error(`Error fetching flag for ${country.name}:`, error);
-          return of(null); // Continuamos con null si ocurre un error
-        })
-      )
+        this.countriesImgService.searchImg(country.name).pipe(
+            catchError((error) => {
+              console.error(`Error fetching image for ${country.name}:`, error);
+              return of(null); // Continuamos con null si ocurre un error
+            })
+        )
     );
 
+    // Usamos forkJoin para esperar que todas las solicitudes de imágenes se completen
     forkJoin(requests).subscribe((responses: any[]) => {
       this.countries = countries.map((country, index) => {
         const response = responses[index];
         const imgUrl = response && response.hits && response.hits.length > 0
-          ? response.hits[0].webformatURL
-          : null;
+            ? response.hits[0].webformatURL
+            : null;
 
         return {
           ...country,
           img: imgUrl
         };
       });
-      console.log('Updated countries:', this.countries);
+
+      this.filteredCountries = [...this.countries];
+      this.isLoading = false;
+      console.log('Updated countries with images:', this.countries);
     });
   }
 
+  isSelected(country: Country): boolean {
+    return this.selectedCountry === country;
+  }
   onCountrySelected(country: Country) {
     this.selectedCountry = country;
     this.isShowCountry = true;
   }
   onCloseCountryDetail() {
     this.isShowCountry = false;
+    this.selectedCountry = null;
+  }
+
+  onContinentsChanged(continents: string[]): void {
+    this.selectedContinents = continents;
+    this.filterCountries();
   }
 }
